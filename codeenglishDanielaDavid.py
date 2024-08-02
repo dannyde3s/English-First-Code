2edb0d260837b2e1688d99e5cc3fe49f76a23db2
dannyde3s <damaris_david@soy.sena.edu.co>
2024-08-02 15:17:30 -0500

    My first code English

---
+++ codeenglishDanielaDavid.py
@@ -0,0 +1,190 @@
+#Dictionaries
+areas={
+    1: "Apps Development",
+    2: "Website Designer",
+    3: "Photo Software",
+    4: "Operating Systems",
+    5: "Blog Designer",
+    6: "Security Software",
+    7: "Social Network Development",
+    8: "Games Development"
+}
+apps_development={
+    "ios apps":"ˈioʊs æps",
+    "android apps":"ˈænˌdrɔɪd æps",
+    "progressive web apps": "prəˈɡrɛsɪv wɛb æps",
+    "single page applications": "ˈsɪŋɡəl peɪʤ ˌæpləˈkeɪʃənz",
+    "windows applications": "ˈwɪndoʊz ˌæpləˈkeɪʃənz",
+    "enterprise resource planning systems": "ˈɛntərˌpraɪz ˈrisɔrs ˈplænɪŋ ˈsɪstəmz",
+    "mobile games": "ˈmoʊbəl ɡeɪmz",
+    "pc and console games": "ˈpiˈsi ænd kənˈsoʊl ɡeɪmz",
+    "restful apis": "ˈrɛstfəl eɪ-pi-aɪz",
+    "customer relationship management systems": "ˈkʌstəmər riˈleɪʃənˌʃɪp ˈmænəʤmənt ˈsɪstəmz",
+    "cloud-based applications": "klaʊd-beɪst ˌæpləˈkeɪʃənz",
+    "bots and scripts": "bɑts ænd skrɪpts",
+    "project management apps": "ˈprɑʤɛkt ˈmænəʤmənt æps",
+    "collaboration tools": "kəˌlæbəˈreɪʃən tulz",
+    "online stores": "ˈɔnˌlaɪn stɔrz"
+}
+website_designer={
+    "custom websites": "ˈkʌstəm ˈwɛbˌsaɪts",
+    "landing pages": "ˈlændɪŋ ˈpeɪʤəz",
+    "e-commerce websites": "i-ˈkɑmərs ˈwɛbˌsaɪts",
+    "portfolio websites": "pɔrtˈfoʊliˌoʊ ˈwɛbˌsaɪts",
+    "blogs": "blɔɡz",
+    "corporate websites": "ˈkɔrpərət ˈwɛbˌsaɪts",
+    "responsive websites": "rɪˈspɑnsɪv ˈwɛbˌsaɪts",
+    "user interface designs": "ˈjuzər ˈɪntərˌfeɪs dɪˈzaɪnz",
+    "user experience designs": "ˈjuzər ɪkˈspɪriəns dɪˈzaɪnz",
+    "content management system integration": "ˈkɑntɛnt ˈmænəʤmənt ˈsɪstəm ˌɪntəˈɡreɪʃən",
+    "website redesigns": "ˈwɛbˌsaɪt ˌridɪˈzaɪnz",
+    "web templates": "wɛb ˈtɛmpləts",
+    "web banners and graphics": "wɛb ˈbænərz ænd ˈɡræfɪks",
+    "interactive web features": "ˌɪntəˈræktɪv wɛb ˈfiʧərz",
+    "web animations": "wɛb ˌænəˈmeɪʃənz"
+}
+photo_software={
+    "photo editing software": "ˈfoʊˌtoʊ ˈɛdətɪŋ ˈsɔfˌtwɛr",
+    "photo management software": "ˈfoʊˌtoʊ ˈmænəʤmənt ˈsɔfˌtwɛr",
+    "digital asset management systems": "ˈdɪʤətəl ˈæˌsɛt ˈmænəʤmənt ˈsɪstəmz",
+    "raw image processors": "rɑ ˈɪməʤ ˈprɑˌsɛsərz",
+    "photo organizing software": "ˈfoʊˌtoʊ ˈɔrɡəˌnaɪzɪŋ ˈsɔfˌtwɛr",
+    "batch processing tools": "bæʧ ˈprɑsɛsɪŋ tulz",
+    "image retouching software": "ˈɪməʤ riˈtʌʧɪŋ ˈsɔfˌtwɛr",
+    "photo printing software": "ˈfoʊˌtoʊ ˈprɪntɪŋ ˈsɔfˌtwɛr",
+    "collage makers": "kəˈlɑʒ ˈmeɪkərz",
+    "high dynamic range software": "haɪ daɪˈnæmɪk reɪnʤ ˈsɔfˌtwɛr",
+    "panorama software": "ˌpænəˈræmə ˈsɔfˌtwɛr",
+    "photo restoration software": "ˈfoʊˌtoʊ ˌrɛstəˈreɪʃən ˈsɔfˌtwɛr",
+    "slide show creators": "slaɪd ʃoʊ kriˈeɪtərz",
+    "photo sharing software": "ˈfoʊˌtoʊ ˈʃɛrɪŋ ˈsɔfˌtwɛr",
+    "image watermarking tools": "ˈɪməʤ ˈwɔtərˌmɑrkɪŋ tulz"
+}
+operating_systems={
+    "cli": "siː-ɛl-aɪ",
+    "daemons": "ˈdiːmənz",
+    "drivers": "ˌmʌltɪˈtɑːskɪŋ",
+    "file system": "faɪl ˈsɪstəm",
+    "kernel": "ˈkɜːnᵊl",
+    "linux": "ˈlɪnəks",
+    "multitasking": "ˌmʌltɪˈtɑːskɪŋ",
+    "patch": "pæʧ",
+    "permissions": "pəˈmɪʃᵊnz",
+    "process management": "ˈprəʊsɛs ˈmænɪʤmənt",
+    "scheduling": "ˈʃɛdjuːlɪŋ",
+    "system calls": "ˈsɪstəm kɔːlz",
+    "user interface": "ˈjuːzər ˈɪntəfeɪs",
+    "virtual memory": "ˈvɜːʧuəl ˈmɛmᵊri",
+    "windows": "ˈwɪndəʊz"
+}
+blog_designer={
+    "custom blog designs": "ˈkʌstəm blɔɡ dɪˈzaɪnz",
+    "blog templates": "blɔɡ ˈtɛmpləts",
+    "blog themes": "blɔɡ θimz",
+    "header and footer designs": "ˈhɛdər ænd ˈfʊtər dɪˈzaɪnz",
+    "post layouts": "poʊst ˈleɪˌaʊts",
+    "visual content integration": "ˈvɪʒəwəl ˈkɑntɛnt ˌɪntəˈɡreɪʃən",
+    "interactive features": "ˌɪntəˈræktɪv ˈfiʧərz",
+    "responsive blog designs": "rɪˈspɑnsɪv blɔɡ dɪˈzaɪnz",
+    "typography and color schemes": "təˈpɑɡrəfi ænd ˈkʌlər skimz",
+    "navigation menus": "ˈnævəˈɡeɪʃən ˈmɛnjuz",
+    "social media integration": "ˈsoʊʃəl ˈmidiə ˌɪntəˈɡreɪʃən",
+    "commenting systems": "ˈkɑmɛntɪŋ ˈsɪstəmz",
+    "seo optimization features": "ˈsioʊ ɑptəməˈzeɪʃən ˈfiʧərz",
+    "content formatting tools": "ˈkɑntɛnt ˈfɔrˌmætɪŋ tulz",
+    "blog branding elements": "blɔɡ ˈbrændɪŋ ˈɛləmənts"
+}
+security_software={
+    "backup and recovery solutions": "ˈbæˌkʌp ænd rɪˈkʌvri səˈluʃənz",
+    "anti-malware software": "ˈænti-ˈmælˌwɛr ˈsɔfˌtwɛr",
+    "firewall software": "ˈfaɪrwɑl ˈsɔfˌtwɛr",
+    "intrusion detection systems": "ɪnˈtruʒən dɪˈtɛkʃən ˈsɪstəmz",
+    "intrusion prevention systems": "ɪnˈtruʒən priˈvɛnʃən ˈsɪstəmz",
+    "virtual private network software": "ˈvɜrʧuəl ˈpraɪvət ˈnɛˌtwɜrk ˈsɔfˌtwɛr",
+    "encryption software": "ɛnˈkrɪpʃən ˈsɔfˌtwɛr",
+    "password managers": "ˈpæˌswɜrd ˈmænəʤərz",
+    "identity and access management systems": "aɪˈdɛntəti ænd ˈækˌsɛs ˈmænəʤmənt ˈsɪstəmz",
+    "endpoint protection software": "ˈɛndˌpɔɪnt prəˈtɛkʃən ˈsɔfˌtwɛr",
+    "data loss prevention (dlp) software": "ˈdeɪtə lɔs priˈvɛnʃən (di-ɛl-pi) ˈsɔfˌtwɛr",
+    "security information and event management systems": "sɪˈkjʊrəti ˌɪnfərˈmeɪʃən ænd ɪˈvɛnt ˈmænəʤmənt ˈsɪstəmz",
+    "network security tools": "ˈnɛˌtwɜrk sɪˈkjʊrəti tulz",
+    "web security software": "wɛb sɪˈkjʊrəti ˈsɔfˌtwɛr",
+    "email security solutions": "iˈmeɪl sɪˈkjʊrəti səˈluʃənz"
+}
+social_network_development={
+    "social networking platforms": "ˈsoʊʃəl ˈnɛˌtwɜrkɪŋ ˈplætˌfɔrmz",
+    "social media apps": "ˈsoʊʃəl ˈmidiə æps",
+    "community forums": "kəmˈjunəti ˈfɔrəmz",
+    "blogging platforms": "ˈblɔɡɪŋ ˈplætˌfɔrmz",
+    "collaborative platforms": "kəˈlæbəˌreɪtɪv ˈplætˌfɔrmz",
+    "professional networking sites": "prəˈfɛʃənəl ˈnɛˌtwɜrkɪŋ saɪts",
+    "dating apps": "ˈdeɪtɪŋ æps",
+    "live streaming platforms": "lɪv ˈstrimɪŋ ˈplætˌfɔrmz",
+    "content sharing sites": "ˈkɑntɛnt ˈʃɛrɪŋ saɪts",
+    "photo sharing apps": "ˈfoʊˌtoʊ ˈʃɛrɪŋ æps",
+    "video sharing platforms": "ˈvɪdioʊ ˈʃɛrɪŋ ˈplætˌfɔrmz",
+    "discussion platforms": "dɪˈskʌʃən ˈplætˌfɔrmz",
+    "event management and planning tools": "ɪˈvɛnt ˈmænəʤmənt ænd ˈplænɪŋ tulz",
+    "interest-based communities": "ˈɪntrəst-beɪst kəmˈjunətiz",
+    "messaging and chat apps": "ˈmɛsɪʤɪŋ ænd ʧæt æps"
+}
+games_development={
+    "pc games": "ˈpiˈsi ɡeɪmz",
+    "console games": "kənˈsoʊl ɡeɪmz",
+    "mobile games": "ˈmoʊbəl ɡeɪmz",
+    "virtual reality games": "ˈvɜrʧuəl ˌriˈæləˌti ɡeɪmz",
+    "augmented reality games": "ɑɡˈmɛntəd ˌriˈæləˌti ɡeɪmz",
+    "browser games": "ˈbraʊzər ɡeɪmz",
+    "indie games": "ˈɪndi ɡeɪmz",
+    "massively multiplayer online games": "ˈmæsɪvli ˈmʌltiˌpleɪər ˈɔnˌlaɪn ɡeɪmz",
+    "educational games": "ˌɛʤəˈkeɪʃənəl ɡeɪmz",
+    "serious games": "ˈsɪriəs ɡeɪmz",
+    "cloud gaming services": "klaʊd ˈɡeɪmɪŋ ˈsɜrvəsəz",
+    "social games": "ˈsoʊʃəl ɡeɪmz",
+    "simulation games": "ˌsɪmjəˈleɪʃən ɡeɪmz",
+    "game engines": "ɡeɪm ˈɛnʤənz",
+    "game development tools": "ɡeɪm dɪˈvɛləpmənt tulz"
+}
+
+# This function is for find the products and its phonetics
+def FuncProducts(productDict):
+    for key_product in productDict.keys():
+        print("-",key_product)
+    phonetic=input("\nType the product to select it (Type '0' to exit): \n").lower()
+    if phonetic == 0:
+        quit()
+    for key_product in productDict.keys():
+        if phonetic == key_product:
+            print("-",productDict[key_product])
+
+# Here start the glossary's interface
+
+print("\nPhonetic of products's Software Development Areas \n\n¿What area do you want to see?\n")
+counter=1
+for i in areas.values():
+    print(counter,"-",i)
+    counter=counter+1
+    
+area=int(input("\nType here the number's area to select it (Type '0' to exit): "))
+if area == 0:
+    quit()
+for key_area in areas.keys():
+    if key_area == area:
+        product= areas[area]
+     
+if product == "Apps Development":
+    FuncProducts(apps_development)
+elif product == "Website Designer":
+    FuncProducts(website_designer)
+elif product == "Photo Software":
+    FuncProducts(photo_software)
+elif product == "Operating Systems":
+    FuncProducts(operating_systems)
+elif product == "Blog Designer":
+    FuncProducts(blog_designer)
+elif product == "Security Software":
+    FuncProducts(security_software)
+elif product == "Social Network Development":
+    FuncProducts(social_network_development)
+elif product == "Games Development":
+    FuncProducts(games_development)
